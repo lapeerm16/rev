0370ae370ab38557fad85b532cd75a9f2f549336
lapeerm16 <144161858+lapeerm16@users.noreply.github.com>
2023-09-11 11:18:44 -0400

    Svetch Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Bdf from '../lib/Bdf.svelte';
+
+	const bdfs = [
+		{ id: 0 },
+		{ id: 1 },
+		{ id: 2 },
+		{ id: 3 }
+	];
+</script>
+
+<main>
+	{#each bdfs as bdf (bdf.id)}
+		<Bdf/>
+	{/each}
+<main>
+
+<style>
+
+</style>
